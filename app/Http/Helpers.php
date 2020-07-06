4026bf94bfd3de331ec22aa1d84ec79bf34df7f7
Aibat1997 <34008555+Aibat1997@users.noreply.github.com>
2020-07-06 17:02:40 +0600

    Update Helpers.php

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -139,4 +139,11 @@ class Helpers
 
         return $result;
     }
+
+    public static function shortDescription($text, $max)
+    {
+        $text_no_tags = strip_tags($text);
+        $short_text = mb_strcut($text_no_tags, 0, $max) . '...';
+        return $short_text;
+    }
 }
